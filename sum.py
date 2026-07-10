6558c494e1126ecd402a053466e3df068775a053
asish358 <gollapudisairangaasish@gmail.com>
2026-07-10 20:08:37 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,12 @@
+def sum(a,b)
+    return a + b
+def main():
+    x = 5
+    y = 10
+    result = sum(x, y)
+    print(f"The sum of {x} and {y} is {result}")
+if __name__ == "__main__":
+    main()
+
+        
+    
