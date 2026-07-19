9bb0bbb1f42c42a6c3078f8e240008e87746686f
asish358 <gollapudisairangaasish@gmail.com>
2026-07-19 19:57:19 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,96 @@
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+'hello world'
+'hello world'
+"hello world"
+'hello world'
+10
+10
+10+20
+30
+10+a
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    10+a
+NameError: name 'a' is not defined
+x+y
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    x+y
+NameError: name 'x' is not defined
+'x'+'y'
+'xy'
+'x'+'y'+'z'
+'xyz'
+print(hello world)
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+print('helloworld')
+helloworld
+print("hello")
+hello
+x=10
+y=20
+print(x+y)
+30
+x=y=z=10
+print(x+y+z)
+30
+print(x)
+10
+print(y)
+10
+>>> print(z)
+10
+>>> #line
+>>> #this is a coment line
+>>> print("hello")
+hello
+>>> x=5
+>>> y=rahul
+Traceback (most recent call last):
+  File "<pyshell#23>", line 1, in <module>
+    y=rahul
+NameError: name 'rahul' is not defined
+>>> print(x)
+5
+>>> print(y)
+10
+>>> y="rahul"
+>>> print(y)
+rahul
+>>> counter=100
+>>> miles=1000.0
+>>> name="kalam"
+>>> print(counter)
+100
+>>> print(name)
+kalam
+>>> print(miles)
+1000.0
+>>> KeyboardInterrupt
+>>> print(type(counter))
+<class 'int'>
+>>> print(type(name))
+<class 'str'>
+>>> print(type(miles))
+<class 'float'>
+>>> x,y,z="orange","banana","cherry"
+>>> print(x)
+orange
+>>> print(y)
+banana
+>>> print(z)
+cherry
+>>> print(xyz)
+Traceback (most recent call last):
+  File "<pyshell#41>", line 1, in <module>
+    print(xyz)
+NameError: name 'xyz' is not defined
+>>> print(x,y,z)
+orange banana cherry
+>>> print(x+y+z)
+orangebananacherry
+>>> x=5
+>>> y="asish"
+>>> print(x,y)
+5 asish
