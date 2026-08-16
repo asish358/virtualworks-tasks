c2e8b81c1346e99fcdfa910d58c0878b0b00fe6b
asish358 <gollapudisairangaasish@gmail.com>
2026-08-16 20:56:37 +0530

    Implement stoneGameIX method in Solution class

---
+++ stone.py
@@ -0,0 +1,10 @@
+class Solution:
+    def stoneGameIX(self, stones: list[int]) -> bool:
+        count = [0, 0, 0]
+        for stone in stones:
+            count[stone % 3] += 1
+            
+        if count[0] % 2 == 0:
+            return count[1] >= 1 and count[2] >= 1
+        else:
+            return abs(count[1] - count[2]) > 2
